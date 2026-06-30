07157c8a70bfdf783364b72b4a5e0445db2bb7db
Bhushan More <morebhushan.92@gmail.com>
2026-06-30 08:52:57 +0530

    Variable

---
+++ Basics/variables.py
@@ -0,0 +1,14 @@
+#Create 5 variables: one of type int, float, string, bool, and NoneType. Print each variable and its type using the type() function.
+value1 = 5
+value2 = 5.2
+text = "Any text you want to write"
+TF1 = True
+TF2 = False
+Nothing = None
+
+print(type(value1))
+print(type(value2))
+print(type(text))
+print(type(TF1))
+print(type(TF2))
+print(type(Nothing))
